97be14f8ed8cf56e77ec0e76428bc714fc8a3e33
anurag gharat <anuraggharat55@gmail.com>
2020-02-07 23:53:48 +0530

    "dependancies sorted"

---
+++ src/components/reuseable/Navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+<nav class="navbar navbar-expand-sm navbar-inverse bg-dark">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+        )
+    }
+}
