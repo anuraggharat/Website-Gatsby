bcdbb1b45a9596473716360c4c530011d1568875
anurag gharat <anuraggharat55@gmail.com>
2020-02-14 22:05:03 +0530

    "footer added"

--- src/components/reuseable/Footer.js
+++ src/components/reuseable/Footer.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
-
+import { Link } from "gatsby"
+import logo from "../../images/gatsby-icon.png"
 export default class Footer extends Component {
     render() {
         return (
-                <footer className="container-fluid">
-                    Footer
-
+                <footer className="container-fluid no-pad no-mar bg-secondary">
+                    <div className="container-fluid no-pad no-mar">
+                        <div className="row no-pad no-mar ">
+                            <div className="col-md-4 text-center pt-5 text-white pb-5">
+                                <img className="footer-logo" src={logo} width="50px" height="50px"></img>
+                                <h4 className="text-white mt-2">LTO Institution Pvt.Ltd .</h4>
+                                <p>Coding made easy.</p>
+                                <div className="container-fluid text-left pl-5" >
+                                <p className="mt-0 mb-0 ml-4">201 Skyparidise,Nerul,Navi-Mumbai.</p>
+                                <p className="mt-0 mb-0 ml-4">Contact: 9876543321</p>
+                                <p className="mt-0 mb-0 ml-4">Email: ltoinstitution@gmail.com</p>
+                                </div>
+                            </div>
+                            <div className="col-md-4"></div>
+                            <div className="col-md-4"></div>
+                        </div>
+                    </div>
+                    <div className="container-fluid credits text-center pt-5 pb-2 bg-dark"> 
+                        <p className="text-white">
+                            Devloped by <Link>Anurag_Gharat</Link> . All rights reserved 2020&copy;
+                        </p>
+                    </div>
                 </footer>
         )
     }
